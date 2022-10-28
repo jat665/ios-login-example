3be1d16bc874b1c72df54b846de365a92ec2a561
Jorge Arindia <jorgearindia@gmail.com>
2022-10-27 21:18:07 -0500

    Initial Commit

---
+++ kripton2/kripton2App.swift
@@ -0,0 +1,17 @@
+//
+//  kripton2App.swift
+//  kripton2
+//
+//  Created by Jorge Arindia on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct kripton2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
